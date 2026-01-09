b82a38cc3558f75b88700d353cf150541c43f354
Marcus <1713450722@qq.com>
2026-01-09 21:11:13 +0800

    全局变量与局部变量的探究

--- C_basics/1/main.c
+++ C_basics/1/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+int num = 10;
+
+void func1() {
+    // 局部变量 - 隐藏了全局变量 num
+    int num = 20;
+    printf("num = %d\n", num);
+}
+
+void func2() {
+    //使用全局变量
+    extern int num;
+    num = 20;
+}
+
+
+
 int main() {
-    printf("Hello, World!\n");
+    func1();
+    printf("num = %d\n", num);
+
+    func2();
+    printf("num = %d\n", num);
+
     return 0;
 }
